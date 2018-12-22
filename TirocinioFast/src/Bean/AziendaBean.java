437a221bc1f7add3222194dc461b62fbc7e94a86
mariog93 <M.GRECO41@studenti.unisa.it>
2018-12-22 01:16:06 +0100

    aziendaBean

--- TirocinioFast/src/Bean/AziendaBean.java
+++ TirocinioFast/src/Bean/AziendaBean.java
@@ -10,7 +10,78 @@ public class AziendaBean implements java.io.Serializable {
 
 	/**Getter e Setter*/
 
+	public String getNome() {
+		return nome;
+	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPartitaIva() {
+		return partitaIva;
+	}
+
+	public void setPartitaIva(String partitaIva) {
+		this.partitaIva = partitaIva;
+	}
+
+	public String getCeo() {
+		return ceo;
+	}
+
+	public void setCeo(String ceo) {
+		this.ceo = ceo;
+	}
+
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDomanda() {
+		return domanda;
+	}
+
+	public void setDomanda(String domanda) {
+		this.domanda = domanda;
+	}
+	
 	/**toString*/
 	
 	@Override
