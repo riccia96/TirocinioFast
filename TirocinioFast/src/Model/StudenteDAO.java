21fd02598b28dda33bba0a286a7a9eba2aff52fd
riccia96 <a.riccio26@studenti.unisa.it>
2018-12-23 16:08:50 +0100

    prova db

--- TirocinioFast/src/Model/StudenteDAO.java
+++ TirocinioFast/src/Model/StudenteDAO.java
@@ -25,7 +25,7 @@ public class StudenteDAO extends AbstractDAO<StudenteBean>{
 			Context initCtx = new InitialContext();
 			Context envCtx = (Context) initCtx.lookup("java:comp/env");
 
-			ds = (DataSource) envCtx.lookup("jdbc/TirocinioFast");
+			ds = (DataSource) envCtx.lookup("jdbc/tirocinioFast");
 
 		} catch (NamingException e) {
 			System.out.println("Error:" + e.getMessage());
