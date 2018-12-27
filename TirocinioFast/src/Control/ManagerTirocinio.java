f2263276a20a9e1360d5f94e10752df706cc30c6
mariog93 <M.GRECO41@studenti.unisa.it>
2018-12-27 19:13:32 +0100

    Fatto il manager Tirocinio

--- TirocinioFast/src/Control/ManagerTirocinio.java
+++ TirocinioFast/src/Control/ManagerTirocinio.java
@@ -1,5 +1,73 @@
 package Control;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Bean.AziendaBean;
+import Model.AziendaDAO;
+
 public class ManagerTirocinio {
 
+	public List<AziendaBean> ricercaAzienda(String nome, String sede) throws SQLException{
+		
+		List<AziendaBean> lAzienda = new ArrayList<AziendaBean>();
+		AziendaDAO azienda = new AziendaDAO();
+		
+		lAzienda.addAll(azienda.doRetrieveAll());
+		
+		for(int i = 0; i < lAzienda.size(); i++){
+			
+			if(!lAzienda.get(i).getNome().equals(nome)){
+				
+				String indirizzo = lAzienda.get(i).getIndirizzo();
+				
+				if(indirizzo.compareTo(sede) != 0)
+					lAzienda.remove(i);
+				
+			}
+		}
+		
+		return lAzienda;
+		
+	}
+	
+	public List<AziendaBean> ricercaAziendaNome(String nome) throws SQLException{
+		
+		List<AziendaBean> lAzienda = new ArrayList<AziendaBean>();
+		AziendaDAO azienda = new AziendaDAO();
+		
+		lAzienda.addAll(azienda.doRetrieveAll());
+		
+		for(int i = 0; i < lAzienda.size(); i++){
+			
+			if(!lAzienda.get(i).getNome().equals(nome))
+				lAzienda.remove(i);
+			
+		}
+		
+		return lAzienda;
+		
+	}
+	
+	public List<AziendaBean> ricercaAziendaSede(String sede) throws SQLException{
+		
+		List<AziendaBean> lAzienda = new ArrayList<AziendaBean>();
+		AziendaDAO azienda = new AziendaDAO();
+		
+		lAzienda.addAll(azienda.doRetrieveAll());
+		
+		for(int i = 0; i < lAzienda.size(); i++){
+			
+			String indirizzo = lAzienda.get(i).getIndirizzo();
+			
+			if(indirizzo.compareTo(sede) != 0)
+				lAzienda.remove(i);
+			
+		}
+		
+		return lAzienda;
+		
+	}
+	
 }
