758a2c5c993721c29ef635883c3c742c2e3c0fa4
riccia96 <a.riccio26@studenti.unisa.it>
2018-12-28 11:25:20 +0100

    manager Utente

--- TirocinioFast/src/Control/ManagerUtente.java
+++ TirocinioFast/src/Control/ManagerUtente.java
@@ -13,24 +13,24 @@ import Model.TutorAccademicoDAO;
 
 public class ManagerUtente {
 
-	public StudenteBean getStudenteDaUsername(String username) throws SQLException{
+	public StudenteBean getStudente(String username) throws SQLException{
 		StudenteDAO sDAO = new StudenteDAO();
 		return sDAO.doRetrieveByKey(username);
-		}
+	}
 	
-	public AziendaBean getAziendaDaUsername(String username) throws SQLException{
+	public AziendaBean getAzienda(String username) throws SQLException{
 		AziendaDAO aDAO = new AziendaDAO();
 		return aDAO.doRetrieveByKey(username);
 	}
 	
-	public ImpiegatoBean getImpiegatoDaUsername(String username) throws SQLException{
-		ImpiegatoDAO iDAO = new ImpiegatoDAO();
-		return iDAO.doRetrieveByKey(username);
-	}
-	
-	public TutorBean getTutorDaUsername(String username) throws SQLException{
+	public TutorBean getTutor(String username) throws SQLException{
 		TutorAccademicoDAO tDAO = new TutorAccademicoDAO();
 		return tDAO.doRetrieveByKey(username);
+	}
+	
+	public ImpiegatoBean getImpiegato(String username) throws SQLException{
+		ImpiegatoDAO iDAO = new ImpiegatoDAO();
+		return iDAO.doRetrieveByKey(username);
 	}
 	
 	public int registraStudente(StudenteBean studente) throws SQLException{
@@ -52,40 +52,20 @@ public class ManagerUtente {
 		AziendaDAO aDAO = new AziendaDAO();
 		return aDAO.doUpdate(azienda);
 	}
+
+	public boolean impostaPasswordTutor(TutorBean tutor) throws SQLException{
+		TutorAccademicoDAO tDAO = new TutorAccademicoDAO();
+		return tDAO.doUpdate(tutor);
+	}
 	
 	public boolean impostaPasswordImpiegato(ImpiegatoBean impiegato) throws SQLException{
 		ImpiegatoDAO iDAO = new ImpiegatoDAO();
 		return iDAO.doUpdate(impiegato);
 	}
 	
-	public boolean impostaPasswordTutor(TutorBean tutor) throws SQLException{
-		TutorAccademicoDAO tDAO = new TutorAccademicoDAO();
-		return tDAO.doUpdate(tutor);
-	}
-	
 	public boolean completaSchedaAzienda(AziendaBean azienda) throws SQLException{
 		AziendaDAO aDAO = new AziendaDAO();
 		return aDAO.doUpdate(azienda);
-	}
-	
-	public boolean reimpostaPasswordStudente(String username, String domanda) throws SQLException{
-		StudenteBean sDAO = this.getStudenteDaUsername(username);
-		return sDAO.getDomanda().equals(domanda);
-	}
-	
-	public boolean reimpostaPasswordAzienda(String username, String domanda) throws SQLException{
-		AziendaBean aDAO = this.getAziendaDaUsername(username);
-		return aDAO.getDomanda().equals(domanda);
-	}
-	
-	public boolean reimpostaPasswordTutor(String username, String domanda) throws SQLException{
-		TutorBean tDAO = this.getTutorDaUsername(username);
-		return tDAO.getDomanda().equals(domanda);
-	}
-	
-	public boolean reimpostaPasswordImpiegato(String username, String domanda) throws SQLException{
-		ImpiegatoBean iDAO = this.getImpiegatoDaUsername(username);
-		return iDAO.getDomanda().equals(domanda);
 	}
 	
 }
