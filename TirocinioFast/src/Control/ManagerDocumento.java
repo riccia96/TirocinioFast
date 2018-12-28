df05e875f4c8a0d6cefd2a1f7ec625ef348e4f64
riccia96 <a.riccio26@studenti.unisa.it>
2018-12-28 12:57:39 +0100

    ManagerDocumento

--- TirocinioFast/src/Control/ManagerDocumento.java
+++ TirocinioFast/src/Control/ManagerDocumento.java
@@ -4,55 +4,87 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import Bean.AziendaBean;
 import Bean.ConvenzioneBean;
 import Bean.QuestionarioAziendaBean;
 import Bean.QuestionarioStudenteBean;
-import Bean.StudenteBean;
 import Bean.TirocinioBean;
-import Bean.TutorBean;
-import Model.AziendaDAO;
 import Model.ConvenzioneDAO;
 import Model.QuestionarioAziendaDAO;
 import Model.QuestionarioStudenteDAO;
-import Model.StudenteDAO;
 import Model.TirocinioDAO;
-import Model.TutorAccademicoDAO;
+
 
 public class ManagerDocumento {
 
 	public boolean Download(String pathDoc){
 		return false;
 	}
-	
+
 	public boolean Upload(String pathDoc){
 		return false;
 	}
-	
+
 	public List<TirocinioBean> richiesteTirocinio() throws SQLException{
-		
+
 		List<TirocinioBean> lTirocinio = new ArrayList<TirocinioBean>();
 		TirocinioDAO tirocinio = new TirocinioDAO();
-		
+
 		lTirocinio.addAll(tirocinio.doRetrieveAll());
-		
+
 		return lTirocinio;
-		
+
 	}
-	
+
 	public List<ConvenzioneBean> convenzioni() throws SQLException{
-		
+
 		List<ConvenzioneBean> lConvenzioni = new ArrayList<ConvenzioneBean>();
 		ConvenzioneDAO convenzione = new ConvenzioneDAO();
-		
+
 		lConvenzioni.addAll(convenzione.doRetrieveAll());
-		
+
 		return lConvenzioni;
-		
+
+	}
+
+	public int compilaTirocinioStudente(TirocinioBean tirocinioS) throws SQLException {
+		TirocinioDAO tirocinio = new TirocinioDAO();
+		return tirocinio.doSave(tirocinioS);
+	}
+
+	public boolean compilaTirocinioAzienda(TirocinioBean tirocinioA) throws SQLException {
+		TirocinioDAO tirocinio = new TirocinioDAO();
+		return tirocinio.doUpdate(tirocinioA);
+	}
+
+	public int compilaConvenzione(ConvenzioneBean convenzioneA) throws SQLException {
+		ConvenzioneDAO convenzione = new ConvenzioneDAO();
+		return convenzione.doSave(convenzioneA);
+	}
+
+	public int compilaQuestionarioStudente(QuestionarioStudenteBean questionarioS) throws SQLException {
+		QuestionarioStudenteDAO questionario = new QuestionarioStudenteDAO();
+		return questionario.doSave(questionarioS);
+	}
+
+	public int compilaQuestionarioAzienda(QuestionarioAziendaBean questionarioA) throws SQLException {
+		QuestionarioAziendaDAO questionario = new QuestionarioAziendaDAO();
+		return questionario.doSave(questionarioA);
+	}
+
+	public boolean convalidaTirocinio(TirocinioBean tirocinioS) throws SQLException {
+		TirocinioDAO tirocinio = new TirocinioDAO();
+		return tirocinio.doUpdate(tirocinioS);
+	}
+
+
+	public boolean convalidaConvenzione(ConvenzioneBean convenzioneA) throws SQLException {
+		ConvenzioneDAO convenzione = new ConvenzioneDAO();
+		return convenzione.doUpdate(convenzioneA);
 	}
 	
+	/*
 	public boolean compilaRichiestaTirocinio(String idStudente, String idAzienda, String idTutor, String annoAccademico, int cfu, boolean handicap) throws SQLException{
-		
+
 		TirocinioDAO tirocinio = new TirocinioDAO();
 		TirocinioBean t = new TirocinioBean();
 		StudenteBean studente = new StudenteBean();
@@ -64,35 +96,35 @@ public class ManagerDocumento {
 		List<StudenteBean> lStudente = new ArrayList<StudenteBean>();
 		List<AziendaBean> lAzienda = new ArrayList<AziendaBean>();
 		List<TutorBean> lTutor = new ArrayList<TutorBean>();
-		
+
 		lStudente.addAll(dStudente.doRetrieveAll());
 		lAzienda.addAll(dAzienda.doRetrieveAll());
 		lTutor.addAll(dTutor.doRetrieveAll());
-		
+
 		for(int i = 0; i < lStudente.size(); i++){
-			
+
 			if(lStudente.get(i).getMatricola() == idStudente){
 				studente = lStudente.get(i);
 				break;
 			}
 		}
-		
+
 		for(int i = 0; i < lAzienda.size(); i++){
-			
+
 			if(lAzienda.get(i).getPartitaIva().equals(idAzienda)){
 				azienda = lAzienda.get(i);
 				break;
 			}
 		}
-		
+
 		for(int i = 0; i < lTutor.size(); i++){
-			
+
 			if(lTutor.get(i).getMatricola().equals(idTutor)){
 				tutor = lTutor.get(i);
 				break;
 			}
 		}
-		
+
 		t.setStudente(studente.getMatricola());
 		t.setAzienda(azienda.getPartitaIva());
 		t.setTutorAccademico(tutor.getMatricola());
@@ -100,36 +132,36 @@ public class ManagerDocumento {
 		t.setAnnoAccademico(annoAccademico);
 		t.setCfu(cfu);
 		t.setHandicap(handicap);
-				
+
 		if(tirocinio.doSave(t) != -1)
 			return true;
-		
+
 		return false;
 	}
-	
+
 	public boolean compilaRichiestaTirocinioAzienda(String idAzienda, String idStudente, String sedeTirocinio, String periodoTirocinio, String accessoLocali,
 			String obiettivoTirocinio, String facilitazioni) throws SQLException{
-		
+
 		TirocinioDAO tirocinio = new TirocinioDAO();
 		TirocinioBean t = new TirocinioBean();
 		ConvenzioneDAO convenzione = new ConvenzioneDAO();
 		ConvenzioneBean c = new ConvenzioneBean();
-		
+
 		c = convenzione.doRetrieveByKey(idAzienda);
-		
+
 		t = tirocinio.doRetrieveByKey(idStudente);
-		
+
 		t.setSedeTirocinio(sedeTirocinio);
 		t.setAccessoLocali(accessoLocali);
 		t.setPeriodoTirocinio(periodoTirocinio);
 		t.setObiettivoTirocinio(obiettivoTirocinio);
 		t.setFacilitazioni(facilitazioni);
-		
+
 		if(c.isConvalida())
 			t.setConvalidaAzienda(true);
 		else
 			t.setConvalidaAzienda(false);
-		
+
 		if(tirocinio.doSave(t) != -1) {
 			return true;
 		}
@@ -137,11 +169,11 @@ public class ManagerDocumento {
 		return false;
 		}
 	}
-	
+
 	public boolean compilaConvenzione(String idAzienda, String luogoNascita, String dataNascita, int numDipendenti, String referente, String telefonoReferente, String emailReferente, String descrizione, String docente) throws SQLException{
 		ConvenzioneDAO convenzione = new ConvenzioneDAO();
 		ConvenzioneBean c = new ConvenzioneBean();
-		
+
 		c.setAzienda(idAzienda);
 		c.setLuogoNascitaCeo(luogoNascita);
 		c.setDataNascitaCeo(dataNascita);
@@ -151,55 +183,55 @@ public class ManagerDocumento {
 		c.setEmailReferente(emailReferente);
 		c.setAttivita(descrizione);
 		c.setTutorAccademico(docente);
-		
+
 		convenzione.doSave(c);
-		
+
 		return true;
 	}
-	
+
 	public boolean compilaQuestionarioStudente(String idStudente, String idAzienda, String idTutor, String titolo, String periodo) throws SQLException{
 		QuestionarioStudenteDAO qsd = new QuestionarioStudenteDAO();
 		QuestionarioStudenteBean qsb = new QuestionarioStudenteBean();
 		TirocinioDAO td = new TirocinioDAO();
 		TirocinioBean tb = new TirocinioBean();
-		
+
 		qsb.setStudente(idStudente);
 		qsb.setAzienda(idAzienda);
 		qsb.setTutorAccademico(idTutor);
 		qsb.setTitolo(titolo);
 		qsb.setPeriodo(periodo);
-		
+
 		qsd.doSave(qsb);
-		
-		/*
+
+		//comment
 		tb.setStudente(idStudente);
 		tb.setAzienda(idAzienda);
 		tb = td.doRetrieveByKey(tb);
 		tb.setQuestionarioStudente(questionarioStudente);
-	*/	
+
 		return true;
 	}
-	
+
 	public boolean compilaQuestionarioAzienda(String idAzienda, String idStudente, String idTutor, String titolo, String periodo, String posizioneRicoperta) throws SQLException{
 		QuestionarioAziendaDAO qad = new QuestionarioAziendaDAO();
 		QuestionarioAziendaBean qab = new QuestionarioAziendaBean();
-		
+
 		qab.setAzienda(idAzienda);
 		qab.setStudente(idStudente);
 		qab.setTutorAccademico(idTutor);
 		qab.setTitoloTirocinio(titolo);
 		qab.setPeriodoTirocinio(periodo);
 		qab.setPosizioneRicoperta(posizioneRicoperta);
-		
+
 		qad.doSave(qab);
-		
-		
-		
+
+
+
 		return true;
 	}
-	/*
+
 	public boolean convalidaTirocinio(String idStudente, String idAzienda, String idTutor, String pathRichiestaTirocinio, String pathQuestionarioStudente, String pathQuestionarioAzienda, String pathQuestionarioAzienda){
-		
+
 	}
-	*/
+	 */
 }
